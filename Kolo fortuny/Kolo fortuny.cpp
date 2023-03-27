1b4b30d0500fa1459296f2188b24148d8ac20a59
Elteys <123510249+Elteys@users.noreply.github.com>
2023-03-28 00:07:07 +0200

    Update Kolo fortuny.cpp

--- Kolo fortuny/Kolo fortuny.cpp
+++ Kolo fortuny/Kolo fortuny.cpp
@@ -1,9 +1,34 @@
-﻿#include <iostream>
+#include <iostream>
+#include <cstdlib>
+#include <Windows.h>
 
 using namespace std;
 
+string slowa[10] = { "pies","kot","klawiatura","tranzystor","kondensator","myszka","komputer","lampka","monitor","glowa"};
+
 int main()
 {
-    cout << "Hello World!";
-}
+    srand(time(NULL));
 
+    int x = rand() % 10;
+
+    int IloscLiter = slowa[x].length();
+
+
+    cout << "------- GENEROWANIE HASLA -------" << endl;
+
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "---------------------------------" << endl;
+        Sleep(100);
+    }
+    system("cls");
+
+    cout << "------- KOLO FORTUNY -------" << endl;
+
+    cout << "liczba liter to: " << IloscLiter << endl;
+
+    
+    
+    return 0;
+}
