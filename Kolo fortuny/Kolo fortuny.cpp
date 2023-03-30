0b1488b93be508da9b3fdf2a3ca8f1508f85bfbf
Elteys <123510249+Elteys@users.noreply.github.com>
2023-03-30 02:33:23 +0200

    Update Kolo fortuny.cpp

--- Kolo fortuny/Kolo fortuny.cpp
+++ Kolo fortuny/Kolo fortuny.cpp
@@ -6,6 +6,27 @@ using namespace std;
 
 string slowa[10] = { "pies","kot","klawiatura","tranzystor","kondensator","myszka","komputer","lampka","monitor","glowa"};
 
+void PolaWypelniania(int x)
+{
+    int* wypelnianie;
+
+    wypelnianie = new int[x];
+
+    for (int i = 0; i < x; i++)
+    {
+        wypelnianie[i] = 0;
+    }
+
+    for (int i = 0; i < x; i++)
+    {
+        if (wypelnianie[i] == 0)
+        {
+            cout << "_ ";
+        }
+    }
+
+}
+
 int main()
 {
     srand(time(NULL));
@@ -23,12 +44,23 @@ int main()
         Sleep(100);
     }
     system("cls");
-
-    cout << "------- KOLO FORTUNY -------" << endl;
+    
+    cout << "--------- KOLO FORTUNY ----------" << endl;
 
     cout << "liczba liter to: " << IloscLiter << endl;
 
+    cout << "---------------------------------" << endl;
+    Sleep(100);
+   
+    cout << "[ ";  PolaWypelniania(IloscLiter); cout << "]" << endl;
+
     
+
+
+
+
+
     
     return 0;
 }
+
