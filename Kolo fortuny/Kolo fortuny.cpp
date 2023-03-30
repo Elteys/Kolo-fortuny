06534ba020c442fa7c5141a1826b5094dc40e596
Elteys <123510249+Elteys@users.noreply.github.com>
2023-03-30 02:51:01 +0200

    Update Kolo fortuny.cpp

--- Kolo fortuny/Kolo fortuny.cpp
+++ Kolo fortuny/Kolo fortuny.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
-#include <cstdlib>
+#include <conio.h>
 #include <Windows.h>
+#include <stdlib.h> /* srand, rand */
+#include <time.h> /* time */
+#include <string>
+#include <typeinfo>
+
+void textColor(char c = 'W')
+{
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    auto colors = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE; //default white gray
+    switch (c) {
+    case 'Y': //yellow
+        colors = FOREGROUND_RED | FOREGROUND_GREEN;
+        break;
+    case 'R':
+        colors = 12;
+        break;
+    case 'G':
+        colors = FOREGROUND_GREEN;
+        break;
+    case 'B':
+        colors = FOREGROUND_BLUE | FOREGROUND_GREEN;
+        break;
+    case 'P':
+        colors = FOREGROUND_RED | BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE;
+    }
+    SetConsoleTextAttribute(hStdOut, colors);
+}
 
 using namespace std;
 
@@ -36,7 +63,7 @@ int main()
     int IloscLiter = slowa[x].length();
     string Litera;
 
-
+    textColor('G');
     cout << "------- GENEROWANIE HASLA -------" << endl;
 
     for(int i = 0; i < 10; i++)
@@ -46,37 +73,38 @@ int main()
     }
     system("cls");
     
+    textColor();
     cout << "********* KOLO FORTUNY **********" << endl;
 
     cout << "liczba liter to: " << IloscLiter << endl;
     Sleep(100);
 
-    cout << "*********************************" << endl;
-    
-    cout << "[ ";  PolaWypelniania(IloscLiter); cout << "]" << endl;
+    cout << "*********************************" << endl << endl;
 
-    cout << "---------------------------------" << endl;
+    textColor('G');
+    cout << "[ ";  PolaWypelniania(IloscLiter); cout << "]" << endl << endl;
 
+    textColor();
     cout << "Podaj pierwsza litere: "; cin >> Litera;
 
 
 
 
-    
-
-
-
-
-
-
-
-
-
-
-
 
     
-    
+
+
+
+
+
+
+
+
+
+
+
+
+  
     return 0;
 }
 
